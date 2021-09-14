34bf963c63cc5c4c0d8be18ed7b24e5b7af7fe9e
edmeighen <edmeighen@mix.wvu.edu>
2021-09-14 16:04:25 -0400

    tried to add routing

--- brickset/src/app/app.module.ts
+++ brickset/src/app/app.module.ts
@@ -10,6 +10,9 @@ import { MySetsComponent } from './cards/my-sets.component';
 import { MinifigsComponent } from './cards/minifigs.component';
 import { DailyBugleComponent } from './cards/daily-bugle.component';
 import { LayoutMainComponent } from './layout/layout-main.component';
+//import { AppRoutingModule } from './app-routing.module';
+import { BodyComponent } from './body/body.component';
+import { ByYearComponent } from './body/by-year.component';
 
 @NgModule({
   declarations: [
@@ -21,10 +24,13 @@ import { LayoutMainComponent } from './layout/layout-main.component';
     MySetsComponent, 
     MinifigsComponent, 
     DailyBugleComponent, 
-    LayoutMainComponent
+    LayoutMainComponent,
+    BodyComponent,
+    ByYearComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ brickset/src/app/body/by-year.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-by-year',
+    templateUrl: './by-year.component.html'
+})
+export class ByYearComponent {}
