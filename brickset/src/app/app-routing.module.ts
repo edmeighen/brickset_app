1299aa34b6f587dd21a0b11214ce1b50d12b3a00
edmeighen <edmeighen@mix.wvu.edu>
2021-09-19 20:09:50 -0400

    routing to different components works

--- brickset/src/app/app-routing.module.ts
+++ brickset/src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { HomeComponent } from './body/home/home.component';
+import { BrowseComponent } from './body/browse/browse.component';
+import { BuyComponent } from './body/buy/buy.component';
+import { ForumComponent } from './body/forum/forum.component';
+import { MySetsComponent } from './body/my-sets/my-sets.component';
 
 const routes: Routes = [
-    
+    { path: 'home', component: HomeComponent },
+    { path: 'browse', component: BrowseComponent },
+    { path: 'buy', component: BuyComponent },
+    { path: 'forum', component: ForumComponent },
+    { path: 'mysets', component: MySetsComponent }
   ];
 
 @NgModule({
