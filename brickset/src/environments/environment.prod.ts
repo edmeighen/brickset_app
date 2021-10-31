3697415c06c0d53510d1abc8fc8d86ef30db3cda
edmeighen <edmeighen@mix.wvu.edu>
2021-10-31 16:52:08 -0400

    added goal 4 for lab assignment 9

--- brickset/src/environments/environment.prod.ts
+++ brickset/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCR7995yJtcvIMHZTXs6Ru3D_Wg90n9Wk8",
+    authDomain: "brickset-b70d8.firebaseapp.com",
+    databaseURL: "https://brickset-b70d8-default-rtdb.firebaseio.com",
+    projectId: "brickset-b70d8",
+    storageBucket: "brickset-b70d8.appspot.com",
+    messagingSenderId: "480486677469",
+    appId: "1:480486677469:web:780d7d239bae091ee7b434",
+    measurementId: "G-0LCKD3N9XD"
+  }
 };
